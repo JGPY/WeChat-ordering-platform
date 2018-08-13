6e16b9ef107414fc10fdc184edf44a9b31596607
jgpy <liubing1994@outlook.com>
2018-08-13 09:45:41 +0800

    demo

---
+++ src/test/java/vip/iotworld/main/LoggerTest.java
@@ -0,0 +1,36 @@
+package vip.iotworld.main;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+
+//    private final Logger logger =  LoggerFactory.getLogger(LoggerTest.class);
+
+//    @Test
+//    public void Test1(){
+//        logger.error("error.....");
+//        logger.debug("debug...");
+//        logger.info("info.....");
+//    }
+
+    @Test
+    public void Test2(){
+
+        String name = "大名";
+        String password = "123456";
+
+        log.error("error.....");
+        log.debug("debug.....");
+        log.info("name:"+name+"password"+password);
+        log.info("namw:{},password:{}",name,password);
+    }
+
+
+}
